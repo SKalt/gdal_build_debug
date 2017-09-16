8ab8755303070ba8faaa1bb276b8374ff4ce6ec8
Steven Kalt <kalt.steven@gmail.com>
2017-09-16 09:35:03 -0400

    added gdal config flag parsing regex

--- gdal_build_debug/gdal_build_debug.py
+++ gdal_build_debug/gdal_build_debug.py
@@ -1,3 +1,18 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
+
+import re
+
+
+# regex to parse args
+flag_parse_regex = re.compile(r'--with(out)?-([\w-]+)(=(\w+))?')
+
+
+def parse_flag(flag):
+    "parse a config argument"
+    match = re.match(flag_parse_regex, flag)
+    if not match:
+        return False
+    else:
+        return match.groups()
