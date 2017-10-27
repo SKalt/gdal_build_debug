8e0027fb6e700132bd8c2c3f10f1cf273416bc05
Unknown <kalt.steven@gmail.com>
2017-10-27 11:05:02 -0400

    Ensured env vars cleared before tests

--- tests/test_dependencies.py
+++ tests/test_dependencies.py
@@ -1,7 +1,8 @@
 import pytest
 import subprocess
 import os
-
+os.unsetenv('WITH')
+os.unsetenv('WITHOUT')
 __location__ = os.path.realpath(
     os.path.join(
         os.getcwd(),
@@ -24,7 +25,7 @@ def options():
     ]
 
 
-def test_single_inclusion(call_cli, options):
+def test_single_inclusion_passes(call_cli, options):
     # print(call_cli + ['--with=GEOS'] + options)
     completed = subprocess.run(
         call_cli + ['--with=GEOS'] + options, stdout=subprocess.PIPE,
@@ -33,7 +34,7 @@ def test_single_inclusion(call_cli, options):
     assert '1 passed' in completed.stdout.decode()
 
 
-def test_multiple_inclusions(call_cli, options):
+def test_multiple_inclusions_pass(call_cli, options):
     completed = subprocess.run(
         call_cli + ['--with=GEOS', '--with=grib'] + options,
         stdout=subprocess.PIPE,
@@ -41,3 +42,36 @@ def test_multiple_inclusions(call_cli, options):
     )
     print(completed.stdout.decode('utf8'))
     assert '2 passed' in completed.stdout.decode()
+
+
+def test_single_exclusion_passes(call_cli, options):
+    completed = subprocess.run(
+        call_cli + ['--without=sqlite'] + options,
+        stdout=subprocess.PIPE,
+        check=True
+    )
+    print(completed.stdout.decode('utf8'))
+    assert '1 passed' in completed.stdout.decode()
+
+
+def test_multiple_exclusions_pass(call_cli, options):
+    completed = subprocess.run(
+        call_cli + ['--without=sqlite', '--without=OpenCL'] + options,
+        stdout=subprocess.PIPE,
+        check=True
+    )
+    print(completed.stdout.decode('utf8'))
+    assert '2 passed' in completed.stdout.decode()
+
+
+def test_multiple_exclusions_fail(call_cli, options):
+    try:
+        completed = subprocess.run(
+            call_cli + ['--without=grib', '--without=geojson'] + options,
+            stdout=subprocess.PIPE,
+            check=True
+        )
+        print(completed.stdout.decode('utf8'))
+        raise AssertionError('expected 2 tests to fail')
+    except subprocess.CalledProcessError as err:
+        assert err.args[0]
